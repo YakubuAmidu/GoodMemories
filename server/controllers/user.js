4b4b2cda3d86a684af146a236ea1e159c320b516
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-10-06 13:55:28 -0700

    Initial commit

---
+++ server/controllers/user.js
@@ -0,0 +1,8 @@
+import bcrypjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import user from "../models/user.js";
+
+export const signin = async (req, res) => {};
+
+export const signup = async (req, res) => {};
---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export default mongoose.model("User", userSchema);
