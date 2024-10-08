fa5ac83317d1844e4e551e5cccf7f7f62d763918
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-10-07 23:39:16 -0700

    Initial commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
