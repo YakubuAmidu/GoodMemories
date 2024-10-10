fb085afd7a867544266c37a084009c5798c75c01
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-10-09 22:23:20 -0700

    Initial commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,18 +4,45 @@ import PostMessage from "../models/postMessage.js";
 // import Post from "../../client/src/components/Posts/Post/Post.js";
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
+
   try {
-    const postMessages = await PostMessage.find();
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+
+    const posts = await PostMessage.find()
+      .sor({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
 
     console.log(postMessages);
 
-    res.status(200).json(postMessages);
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-export const getPostsBySearch = async () => {};
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, "i");
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getPost = async (req, res) => {
   const { id } = req.params;
